f139a0c319841b3fc735a121eb918d0c7b64576e
NogumaCode <111834765+NogumaCode@users.noreply.github.com>
2025-01-23 13:47:12 +0900

    clerk setting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 
 export default function Home() {
   return (
-    <div></div>
+    <div>
+      <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+    </div>
   );
 }
